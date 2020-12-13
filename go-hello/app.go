964924d00ff4a7c3dfc6c899423315dde5448aef
Marco Antonioni <marcoantonioni@users.noreply.github.com>
2020-12-13 14:20:58 +0100

    Update app.go

--- go-hello/app.go
+++ go-hello/app.go
@@ -21,9 +21,9 @@ func rootHandler(response http.ResponseWriter, request *http.Request) {
 
     switch *lang {
       case "en":
-        fmt.Fprintf(response, "Hello %s!. Welcome!\n", request.URL.Path[1:])
+        fmt.Fprintf(response, "Hello %s!. Welcome from GO in git repo !\n", request.URL.Path[1:])
       case "es":
-        fmt.Fprintf(response, "Hola %s!. Bienvenido!\n", request.URL.Path[1:])
+        fmt.Fprintf(response, "Hola %s!. Bienvenido desde GO en git repo !\n", request.URL.Path[1:])
       default:
         fmt.Fprintf(response, "Error! unknown lang option -> %s\n", *lang)
   }
