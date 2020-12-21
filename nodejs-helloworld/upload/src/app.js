c75d0b454bd0d8d4408f7d2663bb769063cc1499
Marco Antonioni <marcoantonioni@users.noreply.github.com>
2020-12-21 12:40:07 +0100

    Update app.js

--- nodejs-helloworld/upload/src/app.js
+++ nodejs-helloworld/upload/src/app.js
@@ -1,1 +1,10 @@
-#
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World from app in branch the-builder !\n');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
