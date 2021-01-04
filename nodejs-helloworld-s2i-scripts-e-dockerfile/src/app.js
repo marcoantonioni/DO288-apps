5a891c305c3eff856a696d9e52cf0d0149d6d942
Marco Antonioni <marcoantonioni@users.noreply.github.com>
2021-01-04 14:27:24 +0100

    Update app.js

--- nodejs-helloworld-s2i-scripts-e-dockerfile/src/app.js
+++ nodejs-helloworld-s2i-scripts-e-dockerfile/src/app.js
@@ -1,1 +1,10 @@
-...
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World from app: nodejs-helloworld-s2i-scripts-e-dockerfile \n');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
