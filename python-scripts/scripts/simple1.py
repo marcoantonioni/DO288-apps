9fab37099966311888013f0a19bd9d6110ce99ae
Marco Antonioni <marcoantonioni@users.noreply.github.com>
2021-01-14 12:29:49 +0100

    Update simple1.py

--- python-scripts/scripts/simple1.py
+++ python-scripts/scripts/simple1.py
@@ -1,5 +1,5 @@
 import sys
 param = sys.argv[1]
 print('\n\n\n     ===== Hello, world from simple1 script in folder scripts ! =====')
-print('      ===== param: ', param)
+print('     ===== param: ', param)
 print('\n\n\n')
