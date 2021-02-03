308da2efbf33ae90035c2645fbe43e362e70441d
Marco Antonioni <marcoantonioni@users.noreply.github.com>
2021-02-03 14:14:32 +0100

    Update app.js

--- nodejs-helloworld-s2i-scripts/app.js
+++ nodejs-helloworld-s2i-scripts/app.js
@@ -1,8 +1,14 @@
 var express = require('express');
+const fs = require('fs')
 app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World from nodejs s2i scripts !\n');
+});
+
+app.get('/my-file', function (req, res) {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('my-file.html').pipe(res)
 });
 
 app.listen(8080, function () {
